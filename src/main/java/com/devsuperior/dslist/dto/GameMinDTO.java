83a3dafaaced40ab66f3425579d5844a2a9d7b61
satoosan <satogui5+github@gmail.com>
2025-08-23 00:12:02 -0300

    consulta sql, projection, busca de games por lista

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,16 +1,17 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
-	private Long id;
-	private String title;
-	private Integer year;
-	private String imgUrl;
-	private String shortDescription;
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
 
-    public GameMinDTO(){
+    public GameMinDTO() {
     }
 
     public GameMinDTO(Game entity) {
@@ -19,6 +20,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
@@ -41,5 +50,4 @@ public class GameMinDTO {
         return shortDescription;
     }
 
-    
 }
